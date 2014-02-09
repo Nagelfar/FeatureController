4a308f8fd00d8d53e7b4196da9b1353cb322c927
Christian Folie <christian.folie@gmail.com>
2014-02-09 23:06:35 +0100

    fetching notifications from server

---
+++ FeatureController/Features/Notification/NotificationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FeatureController.Features.Notification
+{
+    public class NotificationController:Controller
+    {
+        public ActionResult List()
+        {
+            var random = new Random();
+
+            var messages = Enumerable.Range(3, random.Next(15))
+                .Select(x=>"Message "+x);
+            
+            return View(messages.ToList());
+        }
+
+    }
+}
